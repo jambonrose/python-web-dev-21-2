b31b1751cf17efd546a04d320611015e9d38f869
Andrew Pinkham <code@andrewsforge.com>
2018-07-23 12:07:33 -0400

    02.02 configure project to use new apps

--- src/config/settings.py
+++ src/config/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # first party
+    "blog.apps.BlogConfig",
+    "organizer.apps.OrganizerConfig",
 ]
 
 MIDDLEWARE = [
