9f4d5f757a29dc0e7a0ac35f258c71422dc4ef2a
Andrew Pinkham <code@andrewsforge.com>
2018-07-23 15:05:17 -0400

    02.02 install and configure Django Extensions
        $ python -m pip install django-extensions
        $ python -m pip freeze | grep django-extensions >> requirements.txt

--- src/config/settings.py
+++ src/config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # third party
+    "django_extensions",
     "rest_framework",
     # first party
     "blog.apps.BlogConfig",
