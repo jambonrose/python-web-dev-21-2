79220cf510d9038881f2f9279108da1c8166cd39
Andrew Pinkham <code@andrewsforge.com>
2018-07-23 15:39:45 -0400

    02.03 define data model classes for web app

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,15 @@
+"""Django data models for news
+
+Django Model Documentation:
+https://docs.djangoproject.com/en/2.1/topics/db/models/
+Django Field Reference:
+https://docs.djangoproject.com/en/2.1/ref/models/fields/
+
+"""
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """Blog post; news article about startups"""
+
+    pass
--- src/organizer/models.py
+++ src/organizer/models.py
@@ -1,3 +1,27 @@
+"""Django data models for organizing startup company data
+
+Django Model Documentation:
+https://docs.djangoproject.com/en/2.1/topics/db/models/
+Django Field Reference:
+https://docs.djangoproject.com/en/2.1/ref/models/fields/
+
+"""
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    """Labels to help categorize data"""
+
+    pass
+
+
+class Startup(models.Model):
+    """Data about a Startup company"""
+
+    pass
+
+
+class NewsLink(models.Model):
+    """Link to external sources about a Startup"""
+
+    pass
