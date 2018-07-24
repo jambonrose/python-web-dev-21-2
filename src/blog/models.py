659a3198536c4c7017f217b6a9fa6389cfb04e77
Andrew Pinkham <code@andrewsforge.com>
2018-07-24 10:50:38 -0400

    02.03 add related fields to Post

--- src/blog/models.py
+++ src/blog/models.py
@@ -6,11 +6,14 @@ Django Field Reference:
 https://docs.djangoproject.com/en/2.1/ref/models/fields/
 https://docs.djangoproject.com/en/2.1/ref/models/fields/#charfield
 https://docs.djangoproject.com/en/2.1/ref/models/fields/#datefield
+https://docs.djangoproject.com/en/2.1/ref/models/fields/#manytomanyfield
 https://docs.djangoproject.com/en/2.1/ref/models/fields/#slugfield
 https://docs.djangoproject.com/en/2.1/ref/models/fields/#textfield
 
 """
 from django.db import models
+
+from organizer.models import Startup, Tag
 
 
 class Post(models.Model):
@@ -20,3 +23,5 @@ class Post(models.Model):
     slug = models.SlugField()
     text = models.TextField()
     pub_date = models.DateField()
+    tags = models.ManyToManyField(Tag)
+    startups = models.ManyToManyField(Startup)
