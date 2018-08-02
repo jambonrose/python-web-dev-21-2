9d18ab1da4ade45c72cf32c45226cf4167633206
Andrew Pinkham <code@andrewsforge.com>
2018-08-02 15:14:43 -0400

    02.08 change settings for new database

--- src/config/settings.py
+++ src/config/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "webdev21videos2",
+        "USER": "webdev21videos2_user",
+        "PASSWORD": "Well, this is a terrible idea",
+        "HOST": "postgres",
+        "PORT": 5432,
     }
 }
 
