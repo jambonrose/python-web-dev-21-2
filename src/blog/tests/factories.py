2df5f2b4ca654a963643fc88a9782bcf72eb04c2
Andrew Pinkham <code@andrewsforge.com>
2018-08-03 16:43:40 -0400

    test: add fields to test factories

--- src/blog/tests/factories.py
+++ src/blog/tests/factories.py
@@ -1,11 +1,22 @@
 """Factory classes for blog models"""
-from factory import DjangoModelFactory
+from factory import DjangoModelFactory, Faker
 
 from ..models import Post
 
 
 class PostFactory(DjangoModelFactory):
     """Factory for Blog Post data"""
+
+    title = Faker(
+        "sentence", nb_words=3, variable_nb_words=True
+    )
+    slug = Faker("slug")
+    text = Faker(
+        "paragraph",
+        nb_sentences=3,
+        variable_nb_sentences=True,
+    )
+    pub_date = Faker("date_this_decade", before_today=True)
 
     class Meta:
         model = Post
--- src/organizer/tests/factories.py
+++ src/organizer/tests/factories.py
@@ -1,11 +1,14 @@
 """Factory classes for organizer models"""
-from factory import DjangoModelFactory
+from factory import DjangoModelFactory, Faker
 
 from ..models import NewsLink, Startup, Tag
 
 
 class TagFactory(DjangoModelFactory):
     """Factory for Tags (labels)"""
+
+    name = Faker("domain_word")
+    slug = Faker("slug")
 
     class Meta:
         model = Tag
@@ -14,12 +17,28 @@ class TagFactory(DjangoModelFactory):
 class StartupFactory(DjangoModelFactory):
     """Factory for startup company data"""
 
+    name = Faker("company")
+    slug = Faker("slug")
+    description = Faker("catch_phrase")
+    founded_date = Faker(
+        "date_this_decade", before_today=True
+    )
+    contact = Faker("company_email")
+    website = Faker("url")
+
     class Meta:
         model = Startup
 
 
 class NewsLinkFactory(DjangoModelFactory):
     """Factory for article links"""
+
+    title = Faker(
+        "sentence", nb_words=3, variable_nb_words=True
+    )
+    slug = Faker("slug")
+    pub_date = Faker("date_this_decade", before_today=True)
+    link = Faker("uri")
 
     class Meta:
         model = NewsLink
