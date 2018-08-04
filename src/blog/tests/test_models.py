82ac46746f64f864c9fa051d298aedcffbc5a5da
Andrew Pinkham <code@andrewsforge.com>
2018-08-04 10:17:37 -0400

    test: Tag, Startup, Post order in QuerySets

--- src/blog/tests/test_models.py
+++ src/blog/tests/test_models.py
@@ -1,9 +1,12 @@
 """Test for blog app"""
+from datetime import date
+
 from django.test import TestCase
 
 from config.test_utils import get_concrete_field_names
 
 from ..models import Post
+from .factories import PostFactory
 
 
 class PostModelTests(TestCase):
@@ -20,3 +23,23 @@ class PostModelTests(TestCase):
             "pub_date",
         ]
         self.assertEqual(field_names, expected_field_names)
+
+    def test_post_list_order(self):
+        """Are posts ordered by primary-key?"""
+        PostFactory(title="b", pub_date=date(2017, 1, 1))
+        PostFactory(title="a", pub_date=date(2016, 1, 1))
+        PostFactory(title="a", pub_date=date(2017, 1, 1))
+        PostFactory(title="d", pub_date=date(2018, 1, 1))
+        post_name_list = [
+            (name, p_date.year)
+            for name, p_date in Post.objects.values_list(
+                "title", "pub_date"
+            )
+        ]
+        expected_name_list = [
+            ("b", 2017),
+            ("a", 2016),
+            ("a", 2017),
+            ("d", 2018),
+        ]
+        self.assertEqual(post_name_list, expected_name_list)
--- src/organizer/tests/test_models.py
+++ src/organizer/tests/test_models.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from config.test_utils import get_concrete_field_names
 
 from ..models import NewsLink, Startup, Tag
+from .factories import StartupFactory, TagFactory
 
 
 class TagModelTests(TestCase):
@@ -14,6 +15,26 @@ class TagModelTests(TestCase):
         field_names = get_concrete_field_names(Tag)
         expected_field_names = ["id", "name", "slug"]
         self.assertEqual(field_names, expected_field_names)
+
+    def test_list_order(self):
+        """Are tags ordered by primary-key?
+
+        This test is actually dependent on the database and whether the
+        field is unique. In SQLite3, the order will be alphabetical if
+        the name field is unique.
+
+        Will pass regardless if/once Meta ordering is defined.
+
+        """
+        TagFactory(name="b")
+        TagFactory(name="D")
+        TagFactory(name="c")
+        TagFactory(name="a")
+        tag_name_list = list(
+            Tag.objects.values_list("name", flat=True)
+        )
+        expected_name_list = ["b", "D", "c", "a"]
+        self.assertEqual(tag_name_list, expected_name_list)
 
 
 class StartupModelTests(TestCase):
@@ -32,6 +53,28 @@ class StartupModelTests(TestCase):
             "website",
         ]
         self.assertEqual(field_names, expected_field_names)
+
+    def test_list_order(self):
+        """Are Startups ordered by primary-key?
+
+        This test is actually dependent on the database and whether the
+        field is unique. In SQLite3, the order will be alphabetical if
+        the name field is unique.
+
+        Will pass regardless if/once Meta ordering is defined.
+
+        """
+        StartupFactory(name="b")
+        StartupFactory(name="D")
+        StartupFactory(name="c")
+        StartupFactory(name="a")
+        startup_name_list = list(
+            Startup.objects.values_list("name", flat=True)
+        )
+        expected_name_list = ["b", "D", "c", "a"]
+        self.assertEqual(
+            startup_name_list, expected_name_list
+        )
 
 
 class NewsLinkModelTests(TestCase):
