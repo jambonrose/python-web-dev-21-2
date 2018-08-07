1bc66f92166a72ae3d9079779590c4faf9137beb
Andrew Pinkham <code@andrewsforge.com>
2018-08-07 17:41:35 -0400

    03.01 use Class-Based view for Hello World

--- src/organizer/views.py
+++ src/organizer/views.py
@@ -1,16 +1,15 @@
 """Views for Organizer App"""
 from django.http import HttpResponse
-from django.views.decorators.http import (
-    require_http_methods
-)
+from django.views import View
 
 
-# equivalent to require_safe decorator
-@require_http_methods(["GET", "HEAD"])
-def hello_world(request):
+class HelloWorld(View):
     """Demonstrate HTTP Request/Response"""
-    # HttpResponse defaults:
-    # - status code of 200
-    # - content-type of "text/html"
-    # - encoding "charset=utf-8"
-    return HttpResponse("Hello World")
+
+    def get(self, request):
+        """Handle GET HTTP method"""
+        # HttpResponse defaults:
+        # - status code of 200
+        # - content-type of "text/html"
+        # - encoding "charset=utf-8"
+        return HttpResponse("Hello World")
