579e22fa135ebfe3027c6db2d60974127c852c9c
Andrew Pinkham <code@andrewsforge.com>
2018-08-07 18:31:08 -0400

    03.02 split out serialization from views

--- src/organizer/views.py
+++ src/organizer/views.py
@@ -11,15 +11,30 @@ from django.views import View
 from .models import Tag
 
 
+def serialize_tag_to_dict(tag):
+    """Tag -> Python Dict"""
+    return dict(id=tag.pk, name=tag.name, slug=tag.slug)
+
+
+def serialize_tag_to_json(tag):
+    """Tag -> JSON"""
+    return json.dumps(serialize_tag_to_dict(tag))
+
+
+def serialize_tag_list_to_json(tag_list):
+    """[Tag] -> JSON"""
+    return json.dumps(
+        [serialize_tag_to_dict(tag) for tag in tag_list]
+    )
+
+
 class TagAPIDetail(View):
     """Return JSON for single Tag object"""
 
     def get(self, request, pk):
         """Handle GET HTTP method"""
         tag = get_object_or_404(Tag, pk=pk)
-        tag_json = json.dumps(
-            dict(id=tag.pk, name=tag.name, slug=tag.slug)
-        )
+        tag_json = serialize_tag_to_json(tag)
         return HttpResponse(
             tag_json, content_type="application/json"
         )
@@ -31,14 +46,7 @@ class TagAPIList(View):
     def get(self, request):
         """Handle GET HTTP method"""
         tag_list = get_list_or_404(Tag)
-        tag_json = json.dumps(
-            [
-                dict(
-                    id=tag.pk, name=tag.name, slug=tag.slug
-                )
-                for tag in tag_list
-            ]
-        )
+        tag_json = serialize_tag_list_to_json(tag_list)
         return HttpResponse(
             tag_json, content_type="application/json"
         )
