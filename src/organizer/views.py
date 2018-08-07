10ba3a878e8f29ba98a42e53ec4db48c4f021d16
Andrew Pinkham <code@andrewsforge.com>
2018-08-07 18:10:23 -0400

    03.02 serialize data into JSON by hand

--- src/organizer/views.py
+++ src/organizer/views.py
@@ -1,15 +1,36 @@
 """Views for Organizer App"""
+import json
+
 from django.http import HttpResponse
 from django.views import View
 
+from .models import Tag
 
-class HelloWorld(View):
-    """Demonstrate HTTP Request/Response"""
+
+class TagAPIDetail(View):
+    """Return JSON for single Tag object"""
+
+    def get(self, request, pk):
+        """Handle GET HTTP method"""
+        tag = Tag.objects.get(pk=pk)
+        tag_json = json.dumps(
+            dict(id=tag.pk, name=tag.name, slug=tag.slug)
+        )
+        return HttpResponse(tag_json)
+
+
+class TagAPIList(View):
+    """Return JSON for multiple Tag objects"""
 
     def get(self, request):
         """Handle GET HTTP method"""
-        # HttpResponse defaults:
-        # - status code of 200
-        # - content-type of "text/html"
-        # - encoding "charset=utf-8"
-        return HttpResponse("Hello World")
+        tag_list = Tag.objects.all()
+        tag_json = json.dumps(
+            [
+                dict(
+                    id=tag.pk, name=tag.name, slug=tag.slug
+                )
+                for tag in tag_list
+            ]
+        )
+        return HttpResponse(tag_json)
