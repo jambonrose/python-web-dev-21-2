4198cbbfd323470a4fd244a36bdc9ce71f24437a
Andrew Pinkham <code@andrewsforge.com>
2018-08-07 14:15:16 -0400

    test: Tag API get detail, get list

--- src/organizer/tests/test_api_views.py
+++ src/organizer/tests/test_api_views.py
@@ -1,10 +1,36 @@
 """Tests for Organizer Views"""
-from test_plus import TestCase
+import json
+
+from test_plus import APITestCase
+
+from config.test_utils import get_instance_data
+
+from .factories import TagFactory
 
 
-class ViewTests(TestCase):
-    """Basic class to test demo views"""
+class TagAPITests(APITestCase):
+    """Test API Views for Tag objects"""
 
-    def test_hello_world(self):
-        """Is there a Hello World Example?"""
-        self.get_check_200("hello_world")
+    maxDiff = None
+
+    @property
+    def response_json(self):
+        """Shortcut to obtain JSON from last response"""
+        return json.loads(self.last_response.content)
+
+    def test_list(self):
+        """Is there a list of Tag objects"""
+        tag_list = TagFactory.create_batch(10)
+        self.get_check_200("api-tag-list")
+        self.assertCountEqual(
+            self.response_json,
+            map(get_instance_data, tag_list),
+        )
+
+    def test_detail(self):
+        """Is there a detail view for a Tag object"""
+        tag = TagFactory()
+        self.get_check_200("api-tag-detail", pk=tag.pk)
+        self.assertEqual(
+            self.response_json, get_instance_data(tag)
+        )
