5128204e5c805e2831e2d060f8b3cbb686c7851c
Andrew Pinkham <code@andrewsforge.com>
2018-08-08 14:38:28 -0400

    test: serialize Startups with StartupSerializer

--- src/organizer/tests/test_serializers.py
+++ src/organizer/tests/test_serializers.py
@@ -1,10 +1,14 @@
 """Tests for Serializers in the Organizer App"""
 from django.test import TestCase
 
-from config.test_utils import get_instance_data
+from config.test_utils import (
+    get_instance_data,
+    lmap,
+    omit_keys,
+)
 
-from ..serializers import TagSerializer
-from .factories import TagFactory
+from ..serializers import StartupSerializer, TagSerializer
+from .factories import StartupFactory, TagFactory
 
 
 class TagSerializerTests(TestCase):
@@ -26,3 +30,40 @@ class TagSerializerTests(TestCase):
         """Does the serializer validate data?"""
         s_tag = TagSerializer(data={})
         self.assertFalse(s_tag.is_valid())
+
+
+class StartupSerializerTests(TestCase):
+    """Test Serialization of Startups in StartupSerializer"""
+
+    def test_serialization(self):
+        """Does an existing Startup serialize correctly?"""
+        tag_list = TagFactory.create_batch(3)
+        startup = StartupFactory(tags=tag_list)
+        s_startup = StartupSerializer(startup)
+        self.assertEqual(
+            omit_keys("tags", s_startup.data),
+            omit_keys("tags", get_instance_data(startup)),
+        )
+        self.assertCountEqual(
+            s_startup.data["tags"],
+            TagSerializer(tag_list, many=True).data,
+        )
+
+    def test_deserialization(self):
+        """Can we deserialize data to a Startup model?"""
+        startup_data = get_instance_data(
+            StartupFactory.build()
+        )
+        tag_dicts = lmap(
+            get_instance_data, TagFactory.build_batch(3)
+        )
+        data = dict(startup_data, tags=tag_dicts)
+        s_startup = StartupSerializer(data=data)
+        self.assertTrue(
+            s_startup.is_valid(), msg=s_startup.errors
+        )
+
+    def test_invalid_deserialization(self):
+        """Does the serializer validate data?"""
+        s_startup = StartupSerializer(data={})
+        self.assertFalse(s_startup.is_valid())
