da31566b91865e91e6014c6380e3e1402f6f9167
Andrew Pinkham <code@andrewsforge.com>
2018-08-08 14:42:28 -0400

    03.03 implement StartupSerializer

--- src/organizer/serializers.py
+++ src/organizer/serializers.py
@@ -3,11 +3,15 @@
 Serializer Documentation
 http://www.django-rest-framework.org/api-guide/serializers/
 http://www.django-rest-framework.org/api-guide/fields/
+http://www.django-rest-framework.org/api-guide/relations/
 """
 from rest_framework.fields import (
     CharField,
+    DateField,
+    EmailField,
     IntegerField,
     SlugField,
+    URLField,
 )
 from rest_framework.serializers import Serializer
 
@@ -18,3 +22,18 @@ class TagSerializer(Serializer):
     id = IntegerField(read_only=True)
     name = CharField(max_length=31)
     slug = SlugField(max_length=31, allow_blank=True)
+
+
+class StartupSerializer(Serializer):
+    """Serialize Startup data"""
+
+    id = IntegerField(read_only=True)
+    name = CharField(max_length=31)
+    slug = SlugField(max_length=31)
+    description = CharField()
+    founded_date = DateField()
+    contact = EmailField()
+    website = URLField(
+        max_length=255  # https://tools.ietf.org/html/rfc3986
+    )
+    tags = TagSerializer(many=True)
