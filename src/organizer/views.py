45f40e60f90803f9d7fee5f508ebebb9f6fb80f6
Andrew Pinkham <code@andrewsforge.com>
2018-08-09 13:01:50 -0400

    03.04 use DRF's APIView and Response in views

--- src/organizer/views.py
+++ src/organizer/views.py
@@ -1,30 +1,30 @@
 """Views for Organizer App"""
-from django.http import JsonResponse
 from django.shortcuts import (
     get_list_or_404,
     get_object_or_404,
 )
-from django.views import View
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Tag
 from .serializers import TagSerializer
 
 
-class TagAPIDetail(View):
+class TagAPIDetail(APIView):
     """Return JSON for single Tag object"""
 
     def get(self, request, pk):
         """Handle GET HTTP method"""
         tag = get_object_or_404(Tag, pk=pk)
         s_tag = TagSerializer(tag)
-        return JsonResponse(s_tag.data)
+        return Response(s_tag.data)
 
 
-class TagAPIList(View):
+class TagAPIList(APIView):
     """Return JSON for multiple Tag objects"""
 
     def get(self, request):
         """Handle GET HTTP method"""
         tag_list = get_list_or_404(Tag)
         s_tag = TagSerializer(tag_list, many=True)
-        return JsonResponse(s_tag.data, safe=False)
+        return Response(s_tag.data)
