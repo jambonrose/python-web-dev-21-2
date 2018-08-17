7e16608d755bf90bb71f8f598831972eef7ecaed
Andrew Pinkham <code@andrewsforge.com>
2018-08-17 14:15:51 -0400

    04.05 shorten Tag list & detail views with render

--- src/organizer/views.py
+++ src/organizer/views.py
@@ -1,7 +1,5 @@
 """Views for Organizer App"""
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
-from django.template import loader
+from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_safe
 from rest_framework.generics import (
     ListAPIView,
@@ -20,20 +18,16 @@ from .serializers import (
 def tag_list(request):
     """Render an HTML template of a list of tags"""
     tag_list = Tag.objects.all()
-    template = loader.get_template("tag/list.html")
     context = {"tag_list": tag_list}
-    html_content = template.render(context)
-    return HttpResponse(html_content)
+    return render(request, "tag/list.html", context)
 
 
 @require_safe
 def tag_detail(request, slug):
     """Render an HTML template of a tags"""
     tag = get_object_or_404(Tag, slug=slug)
-    template = loader.get_template("tag/detail.html")
     context = {"tag": tag}
-    html_content = template.render(context)
-    return HttpResponse(html_content)
+    return render(request, "tag/detail.html", context)
 
 
 class TagAPIDetail(RetrieveAPIView):
