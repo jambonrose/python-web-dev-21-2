eca9c0cb238adff434913843b8b149c0c9f95759
Andrew Pinkham <code@andrewsforge.com>
2018-08-16 20:56:20 -0400

    03.06 simplify views with DRF generics

--- src/organizer/views.py
+++ src/organizer/views.py
@@ -1,62 +1,38 @@
 """Views for Organizer App"""
-from django.shortcuts import (
-    get_list_or_404,
-    get_object_or_404,
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView,
 )
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from .models import Startup, Tag
 from .serializers import StartupSerializer, TagSerializer
 
 
-class TagAPIDetail(APIView):
+class TagAPIDetail(RetrieveAPIView):
     """Return JSON for single Tag object"""
 
-    def get(self, request, slug):
-        """Handle GET HTTP method"""
-        tag = get_object_or_404(Tag, slug=slug)
-        s_tag = TagSerializer(
-            tag, context={"request": request}
-        )
-        return Response(s_tag.data)
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    lookup_field = "slug"
 
 
-class TagAPIList(APIView):
+class TagAPIList(ListAPIView):
     """Return JSON for multiple Tag objects"""
 
-    def get(self, request):
-        """Handle GET HTTP method"""
-        tag_list = get_list_or_404(Tag)
-        s_tag = TagSerializer(
-            tag_list,
-            many=True,
-            context={"request": request},
-        )
-        return Response(s_tag.data)
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
 
 
-class StartupAPIDetail(APIView):
+class StartupAPIDetail(RetrieveAPIView):
     """Return JSON for single Startup object"""
 
-    def get(self, request, slug):
-        """Handle GET HTTP method"""
-        startup = get_object_or_404(Startup, slug=slug)
-        s_startup = StartupSerializer(
-            startup, context={"request": request}
-        )
-        return Response(s_startup.data)
+    queryset = Startup.objects.all()
+    serializer_class = StartupSerializer
+    lookup_field = "slug"
 
 
-class StartupAPIList(APIView):
+class StartupAPIList(ListAPIView):
     """Return JSON for multiple Startup objects"""
 
-    def get(self, request):
-        """Handle GET HTTP method"""
-        startup_list = get_list_or_404(Startup)
-        s_startup = StartupSerializer(
-            startup_list,
-            many=True,
-            context={"request": request},
-        )
-        return Response(s_startup.data)
+    queryset = Startup.objects.all()
+    serializer_class = StartupSerializer
