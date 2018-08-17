75340b7bf1bd28e9fac88301118e202531acbc71
Andrew Pinkham <code@andrewsforge.com>
2018-08-16 21:56:32 -0400

    03.08 prefix API urls

--- src/config/urls.py
+++ src/config/urls.py
@@ -5,8 +5,12 @@ from django.urls import include, path
 from blog import urls as blog_urls
 from organizer import urls as organizer_urls
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
+api_urls = [
     path("", include(organizer_urls)),
     path("", include(blog_urls)),
+]
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/v1/", include(api_urls)),
 ]
