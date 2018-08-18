e744c8b4e2f3f7e62ab1462f01fb9d686c2c65f0
Andrew Pinkham <code@andrewsforge.com>
2018-08-18 13:54:01 -0400

    05.01 handle POST in TagAPIList view to create Tag

--- src/organizer/views.py
+++ src/organizer/views.py
@@ -5,6 +5,11 @@ from rest_framework.generics import (
     ListAPIView,
     RetrieveAPIView,
 )
+from rest_framework.response import Response
+from rest_framework.status import (
+    HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST,
+)
 
 from .models import NewsLink, Startup, Tag
 from .serializers import (
@@ -55,6 +60,20 @@ class TagAPIList(ListAPIView):
 
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+
+    def post(self, request):
+        """Create new Tag upon POST"""
+        s_tag = TagSerializer(
+            data=request.data, context={"request": request}
+        )
+        if s_tag.is_valid():
+            s_tag.save()
+            return Response(
+                s_tag.data, status=HTTP_201_CREATED
+            )
+        return Response(
+            s_tag.errors, status=HTTP_400_BAD_REQUEST
+        )
 
 
 class StartupAPIDetail(RetrieveAPIView):
