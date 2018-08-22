e74f375cc5aba147f482c2dcf1bcecb569bd2e5f
Andrew Pinkham <code@andrewsforge.com>
2018-08-22 09:41:50 -0700

    05.05 ViewSet action to relate Startups to Tags

--- src/organizer/viewsets.py
+++ src/organizer/viewsets.py
@@ -1,4 +1,11 @@
 """Viewsets for the Organizer App"""
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.status import (
+    HTTP_204_NO_CONTENT,
+    HTTP_400_BAD_REQUEST,
+)
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Startup, Tag
@@ -19,3 +26,24 @@ class StartupViewSet(ModelViewSet):
     lookup_field = "slug"
     queryset = Startup.objects.all()
     serializer_class = StartupSerializer
+
+    @action(detail=True, methods=["HEAD", "GET", "POST"])
+    def tags(self, request, slug=None):
+        """Relate a POSTed Tag to Startup in URI"""
+        startup = self.get_object()
+        if request.method in ("HEAD", "GET"):
+            s_tag = TagSerializer(
+                startup.tags,
+                many=True,
+                context={"request": request},
+            )
+            return Response(s_tag.data)
+        tag_slug = request.data.get("slug")
+        if not tag_slug:
+            return Response(
+                "Slug of Tag must be specified",
+                status=HTTP_400_BAD_REQUEST,
+            )
+        tag = get_object_or_404(Tag, slug__iexact=tag_slug)
+        startup.tags.add(tag)
+        return Response(status=HTTP_204_NO_CONTENT)
