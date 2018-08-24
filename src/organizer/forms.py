a5309859ac52f9861fefda379add716565330bdd
Andrew Pinkham <code@andrewsforge.com>
2018-08-23 20:38:05 -0700

    06.02 use ModelForm superclass for TagForm

--- src/organizer/forms.py
+++ src/organizer/forms.py
@@ -1,23 +1,12 @@
 """Forms for the Organizer app"""
 from django.core.exceptions import ValidationError
-from django.forms import CharField, Form, SlugField
+from django.forms import ModelForm
 
 from .models import Tag
 
 
-class TagForm(Form):
-    """HTML form for Tag objects"""
-
-    name = CharField(max_length=31)
-    slug = SlugField(
-        help_text="A label for URL config",
-        max_length=31,
-        required=False,
-    )
-
-    def clean_name(self):
-        """Ensure Tag name is always lowercase"""
-        return self.cleaned_data["name"].lower()
+class SlugCleanMixin:
+    """Mixin class to ensure slug field is not create"""
 
     def clean_slug(self):
         """Ensure slug is not 'create'
@@ -35,9 +24,14 @@ class TagForm(Form):
             )
         return slug
 
-    def save(self):
-        """Save the data in the bound form"""
-        return Tag.objects.create(
-            name=self.cleaned_data["name"],
-            slug=self.cleaned_data["slug"],
-        )
+
+class TagForm(ModelForm):
+    """HTML form for Tag objects"""
+
+    class Meta:
+        model = Tag
+        fields = "__all__"  # name only, no slug!
+
+    def clean_name(self):
+        """Ensure Tag name is always lowercase"""
+        return self.cleaned_data["name"].lower()
