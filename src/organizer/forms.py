edf864a7c37e7911088d3262f9e40af998403791
Andrew Pinkham <code@andrewsforge.com>
2018-08-23 20:10:25 -0700

    06.01 use clean field methods in TagForm

--- src/organizer/forms.py
+++ src/organizer/forms.py
@@ -1,4 +1,5 @@
 """Forms for the Organizer app"""
+from django.core.exceptions import ValidationError
 from django.forms import CharField, Form, SlugField
 
 from .models import Tag
@@ -13,6 +14,26 @@ class TagForm(Form):
         max_length=31,
         required=False,
     )
+
+    def clean_name(self):
+        """Ensure Tag name is always lowercase"""
+        return self.cleaned_data["name"].lower()
+
+    def clean_slug(self):
+        """Ensure slug is not 'create'
+
+        This is an oversimplification!!! See the following
+        link for how to raise the error correctly.
+
+        https://docs.djangoproject.com/en/2.1/ref/forms/validation/#raising-validationerror
+
+        """
+        slug = self.cleaned_data["slug"]
+        if slug == "create":
+            raise ValidationError(
+                "Slug may not be 'create'."
+            )
+        return slug
 
     def save(self):
         """Save the data in the bound form"""
