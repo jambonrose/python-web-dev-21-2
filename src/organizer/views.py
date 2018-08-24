b105a2479c7c66681bee311ab89741c9655a4504
Andrew Pinkham <code@andrewsforge.com>
2018-08-24 09:39:29 -0700

    06.03 code Tag create, update, and delete views

--- src/organizer/views.py
+++ src/organizer/views.py
@@ -1,6 +1,14 @@
 """Views for Organizer App"""
+from django.shortcuts import (
+    get_object_or_404,
+    redirect,
+    render,
+)
+from django.urls import reverse
+from django.views import View
 from django.views.generic import DetailView, ListView
 
+from .forms import TagForm
 from .models import Startup, Tag
 
 
@@ -16,6 +24,71 @@ class TagDetail(DetailView):
 
     queryset = Tag.objects.all()
     template_name = "tag/detail.html"
+
+
+class TagCreate(View):
+    """Create new Tags via HTML form"""
+
+    def get(self, request):
+        """Display an HTML form"""
+        context = {"form": TagForm(), "update": False}
+        return render(request, "tag/form.html", context)
+
+    def post(self, request):
+        """Handle Form submission: save Tag"""
+        tform = TagForm(request.POST)
+        if tform.is_valid():
+            tag = tform.save()
+            return redirect(tag.get_absolute_url())
+        # invalid data; show form with errors
+        context = {"form": tform, "update": False}
+        return render(request, "tag/form.html", context)
+
+
+class TagUpdate(View):
+    """Update a Tag via HTML form"""
+
+    def get(self, request, slug):
+        """Display an HTML form with pre-filled data"""
+        tag = get_object_or_404(Tag, slug=slug)
+        context = {
+            "tag": tag,
+            "form": TagForm(instance=tag),
+            "update": True,
+        }
+        return render(request, "tag/form.html", context)
+
+    def post(self, request, slug):
+        """Handle Form submission: save Tag"""
+        tag = get_object_or_404(Tag, slug=slug)
+        tform = TagForm(request.POST, instance=tag)
+        if tform.is_valid():
+            tag = tform.save()
+            return redirect(tag.get_absolute_url())
+        # invalid data; show form with errors
+        context = {
+            "tag": tag,
+            "form": tform,
+            "update": True,
+        }
+        return render(request, "tag/form.html", context)
+
+
+class TagDelete(View):
+    """Confirm and delete a Tag via HTML Form"""
+
+    def get(self, request, slug):
+        """Display an HTML form to confirm removal"""
+        tag = get_object_or_404(Tag, slug=slug)
+        return render(
+            request, "tag/confirm_delete.html", {"tag": tag}
+        )
+
+    def post(self, request, slug):
+        """Delete Tag"""
+        tag = get_object_or_404(Tag, slug=slug)
+        tag.delete()
+        return redirect(reverse("tag_list"))
 
 
 class StartupList(ListView):
