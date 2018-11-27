51e395b7d02224efdb85932e3c7625125fb8f048
Andrew Pinkham <code@andrewsforge.com>
2018-11-27 11:47:46 -0500

    test: PostSerializer changes M2M of Post

--- src/blog/tests/test_serializers.py
+++ src/blog/tests/test_serializers.py
@@ -1,18 +1,14 @@
 """Tests for Serializers in the Blog App"""
 from functools import partial
+from random import randint
 
 from django.test import TestCase
 
 from config.test_utils import (
     context_kwarg,
     get_instance_data,
-    lmap,
     omit_keys,
     reverse,
-)
-from organizer.serializers import (
-    StartupSerializer,
-    TagSerializer,
 )
 from organizer.tests.factories import (
     StartupFactory,
@@ -57,44 +53,194 @@ class PostSerializerTests(TestCase):
         )
         self.assertCountEqual(
             s_post.data["tags"],
-            TagSerializer(
-                tag_list, many=True, **post_ctxt
-            ).data,
+            [
+                reverse(
+                    "api-tag-detail",
+                    slug=tag.slug,
+                    full=True,
+                )
+                for tag in tag_list
+            ],
         )
         self.assertCountEqual(
             s_post.data["startups"],
-            StartupSerializer(
-                startup_list, many=True, **post_ctxt
-            ).data,
+            [
+                reverse(
+                    "api-startup-detail",
+                    slug=startup.slug,
+                    full=True,
+                )
+                for startup in startup_list
+            ],
         )
 
     def test_deserialization(self):
         """Can we deserialize data to a Post model?"""
-        tag_dicts = lmap(
-            get_instance_data, TagFactory.build_batch(2)
+        tag_list = TagFactory.create_batch(randint(1, 10))
+        tag_urls = [
+            reverse("api-tag-detail", slug=tag.slug)
+            for tag in tag_list
+        ]
+        startup_list = StartupFactory.create_batch(
+            randint(1, 10)
         )
-        startup_tag_dicts = lmap(
-            get_instance_data, TagFactory.build_batch(2)
-        )
-        startup_data = remove_m2m(
-            get_instance_data(StartupFactory.build())
-        )
+        startup_urls = [
+            reverse("api-startup-detail", slug=startup.slug)
+            for startup in startup_list
+        ]
         post_data = remove_m2m(
             get_instance_data(PostFactory.build())
         )
         data = dict(
             **post_data,
-            tags=tag_dicts,
-            startups=[
-                {**startup_data, "tags": startup_tag_dicts}
-            ],
+            tags=tag_urls,
+            startups=startup_urls,
         )
         s_post = PostSerializer(
-            data=data, **context_kwarg(f"/api/v1/startup/")
+            data=data,
+            **context_kwarg(
+                reverse("api-post-list", full=True)
+            ),
         )
         self.assertTrue(
             s_post.is_valid(), msg=s_post.errors
         )
+        post = s_post.save()
+        self.assertCountEqual(tag_list, post.tags.all())
+        self.assertCountEqual(
+            startup_list, post.startups.all()
+        )
+
+    def test_deserialization_update(self):
+        """Can we deserialize to an existing Post model?"""
+        tag_list = TagFactory.create_batch(randint(1, 10))
+        tag_urls = [
+            reverse("api-tag-detail", slug=tag.slug)
+            for tag in tag_list
+        ]
+        startup_list = StartupFactory.create_batch(
+            randint(1, 10)
+        )
+        startup_urls = [
+            reverse("api-startup-detail", slug=startup.slug)
+            for startup in startup_list
+        ]
+        post = PostFactory(
+            title="first", tags=TagFactory.create_batch(3)
+        )
+        post_data = remove_m2m(get_instance_data(post))
+        data = dict(
+            post_data,
+            title="second",
+            tags=tag_urls,
+            startups=startup_urls,
+        )
+        s_post = PostSerializer(
+            post,
+            data=data,
+            **context_kwarg(
+                reverse(
+                    "api-post-detail",
+                    year=post.pub_date.year,
+                    month=post.pub_date.month,
+                    slug=post.slug,
+                    full=True,
+                )
+            ),
+        )
+        self.assertTrue(
+            s_post.is_valid(), msg=s_post.errors
+        )
+        post = s_post.save()
+        self.assertEqual("second", post.title)
+        self.assertCountEqual(tag_list, post.tags.all())
+        self.assertCountEqual(
+            startup_list, post.startups.all()
+        )
+
+    def test_deserialization_partial_update(self):
+        """Can we partially deserialize data to a Post model?"""
+        tag_list = TagFactory.create_batch(randint(1, 10))
+        tag_urls = [
+            reverse("api-tag-detail", slug=tag.slug)
+            for tag in tag_list
+        ]
+        startup_list = StartupFactory.create_batch(
+            randint(1, 10)
+        )
+        startup_urls = [
+            reverse("api-startup-detail", slug=startup.slug)
+            for startup in startup_list
+        ]
+        post = PostFactory(
+            title="first", tags=TagFactory.create_batch(3)
+        )
+        s_post = PostSerializer(
+            instance=post,
+            data=dict(
+                title="second",
+                # necessary due to bug in DRF
+                # https://github.com/encode/django-rest-framework/issues/6341
+                slug=post.slug,
+                pub_date=post.pub_date,
+                # remove above once DRF fixed
+                tags=tag_urls,
+                startups=startup_urls,
+            ),
+            partial=True,
+            **context_kwarg(
+                reverse(
+                    "api-post-detail",
+                    year=post.pub_date.year,
+                    month=post.pub_date.month,
+                    slug=post.slug,
+                    full=True,
+                )
+            ),
+        )
+        self.assertTrue(
+            s_post.is_valid(), msg=s_post.errors
+        )
+        post = s_post.save()
+        self.assertEqual("second", post.title)
+        self.assertCountEqual(tag_list, post.tags.all())
+        self.assertCountEqual(
+            startup_list, post.startups.all()
+        )
+
+    def test_deserialization_partial_update_unknown_m2m(
+        self
+    ):
+        """Can we partially deserialize data to a Post model?"""
+        tag_urls = [
+            reverse("api-tag-detail", slug=tag.slug)
+            for tag in TagFactory.build_batch(
+                randint(1, 10)
+            )
+        ]
+        post = PostFactory()
+        s_post = PostSerializer(
+            instance=post,
+            data=dict(
+                # necessary due to bug in DRF
+                # https://github.com/encode/django-rest-framework/issues/6341
+                slug=post.slug,
+                pub_date=post.pub_date,
+                # remove above once DRF fixed
+                tags=tag_urls,
+            ),
+            partial=True,
+            **context_kwarg(
+                reverse(
+                    "api-post-detail",
+                    year=post.pub_date.year,
+                    month=post.pub_date.month,
+                    slug=post.slug,
+                    full=True,
+                )
+            ),
+        )
+        self.assertFalse(s_post.is_valid())
 
     def test_invalid_deserialization(self):
         """Does the serializer validate data?"""
