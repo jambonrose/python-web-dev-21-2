69a03dfd3444d617aa10b7d10bb816b56d6c419f
Andrew Pinkham <code@andrewsforge.com>
2018-11-27 10:54:18 -0500

    🧁 Reverse API URL in PostSerializer

--- src/blog/serializers.py
+++ src/blog/serializers.py
@@ -6,7 +6,11 @@ http://www.django-rest-framework.org/api-guide/fields/
 http://www.django-rest-framework.org/api-guide/relations/
 """
 
-from rest_framework.serializers import ModelSerializer
+from rest_framework.reverse import reverse
+from rest_framework.serializers import (
+    ModelSerializer,
+    SerializerMethodField,
+)
 
 from organizer.serializers import (
     StartupSerializer,
@@ -19,9 +23,22 @@ from .models import Post
 class PostSerializer(ModelSerializer):
     """Serialize Post data"""
 
+    url = SerializerMethodField()
     tags = TagSerializer(many=True)
     startups = StartupSerializer(many=True)
 
     class Meta:
         model = Post
         fields = "__all__"
+
+    def get_url(self, post):
+        """Return full API URL for serialized POST object"""
+        return reverse(
+            "api-post-detail",
+            kwargs=dict(
+                year=post.pub_date.year,
+                month=post.pub_date.month,
+                slug=post.slug,
+            ),
+            request=self.context["request"],
+        )
