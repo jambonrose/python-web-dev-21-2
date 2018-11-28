5958b995f00ba6eca5700f4bc9d8e7fb3235a9d4
Andrew Pinkham <code@andrewsforge.com>
2018-11-28 15:59:19 -0500

    🎂 Advanced: use ViewSet and Router for Post objs

--- src/blog/views.py
+++ src/blog/views.py
@@ -2,13 +2,8 @@
 from django.shortcuts import get_object_or_404, render
 from django.views import View
 from django.views.generic import ListView
-from rest_framework.generics import (
-    ListCreateAPIView,
-    RetrieveUpdateDestroyAPIView,
-)
 
 from .models import Post
-from .serializers import PostSerializer
 
 
 class PostList(ListView):
@@ -32,37 +27,3 @@ class PostDetail(View):
         return render(
             request, "post/detail.html", {"post": post}
         )
-
-
-class PostAPIList(ListCreateAPIView):
-    """Return data for multiple Post objects"""
-
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-class PostAPIDetail(RetrieveUpdateDestroyAPIView):
-    """Return data for a single Post object"""
-
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-    def get_object(self):
-        """Override DRF's generic method
-
-        http://www.cdrf.co/3.7/rest_framework.generics/ListAPIView.html#get_object
-        """
-        month = self.kwargs.get("month")
-        year = self.kwargs.get("year")
-        slug = self.kwargs.get("slug")
-
-        queryset = self.filter_queryset(self.get_queryset())
-
-        post = get_object_or_404(
-            queryset,
-            pub_date__year=year,
-            pub_date__month=month,
-            slug=slug,
-        )
-        self.check_object_permissions(self.request, post)
-        return post
