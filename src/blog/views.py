f014d6499b1637422119639b6551dc49c7ca84d3
Andrew Pinkham <code@andrewsforge.com>
2018-11-28 15:57:03 -0500

    🧁 Replace Post API logic with generic behavior

--- src/blog/views.py
+++ src/blog/views.py
@@ -3,15 +3,8 @@ from django.shortcuts import get_object_or_404, render
 from django.views import View
 from django.views.generic import ListView
 from rest_framework.generics import (
-    ListAPIView,
-    RetrieveAPIView,
-)
-from rest_framework.response import Response
-from rest_framework.status import (
-    HTTP_200_OK,
-    HTTP_201_CREATED,
-    HTTP_204_NO_CONTENT,
-    HTTP_400_BAD_REQUEST,
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
 )
 
 from .models import Post
@@ -41,28 +34,14 @@ class PostDetail(View):
         )
 
 
-class PostAPIList(ListAPIView):
+class PostAPIList(ListCreateAPIView):
     """Return data for multiple Post objects"""
 
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-    def post(self, request):
-        """Create new Post upon POST"""
-        s_post = self.serializer_class(
-            data=request.data, context={"request": request}
-        )
-        if s_post.is_valid():
-            s_post.save()
-            return Response(
-                s_post.data, status=HTTP_201_CREATED
-            )
-        return Response(
-            s_post.errors, status=HTTP_400_BAD_REQUEST
-        )
 
-
-class PostAPIDetail(RetrieveAPIView):
+class PostAPIDetail(RetrieveUpdateDestroyAPIView):
     """Return data for a single Post object"""
 
     queryset = Post.objects.all()
@@ -87,64 +66,3 @@ class PostAPIDetail(RetrieveAPIView):
         )
         self.check_object_permissions(self.request, post)
         return post
-
-    def put(self, request, *args, **kwargs):
-        """Update Post object with new data from PUT
-
-        Type signature could also be:
-            def put(self, request, year, month, slug)
-
-        Given that we don't use any of the key-word
-        arguments, we simplify the signature with Python's
-        args/kwargs signature.
-        """
-        post = self.get_object()
-        s_post = self.serializer_class(
-            post,
-            data=request.data,
-            context={"request": request},
-        )
-        if s_post.is_valid():
-            s_post.save()
-            return Response(s_post.data, status=HTTP_200_OK)
-        return Response(
-            s_post.errors, status=HTTP_400_BAD_REQUEST
-        )
-
-    def patch(self, request, *args, **kwargs):
-        """Update Post object with new data from PATCH
-
-        Type signature could also be:
-            def patch(self, request, year, month, slug)
-
-        Given that we don't use any of the key-word
-        arguments, we simplify the signature with Python's
-        args/kwargs signature.
-        """
-        post = self.get_object()
-        s_post = self.serializer_class(
-            post,
-            data=request.data,
-            partial=True,
-            context={"request": request},
-        )
-        if s_post.is_valid():
-            s_post.save()
-            return Response(s_post.data, status=HTTP_200_OK)
-        return Response(
-            s_post.errors, status=HTTP_400_BAD_REQUEST
-        )
-
-    def delete(self, request, *args, **kwargs):
-        """DELETE the Post
-
-        Type signature could also be:
-            def delete(self, request, year, month, slug)
-
-        Given that we don't use any of the key-word
-        arguments, we simplify the signature with Python's
-        args/kwargs signature.
-        """
-        post = self.get_object()
-        post.delete()
-        return Response(status=HTTP_204_NO_CONTENT)
