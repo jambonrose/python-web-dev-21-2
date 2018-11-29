1a8f68b5a41d6a1390dfccb5db343739151edf4c
Andrew Pinkham <code@andrewsforge.com>
2018-11-29 16:20:49 -0500

    🧁 Code views to create, update, delete Posts

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,13 +1,32 @@
 """URL paths for Blog App"""
 from django.urls import path
 
-from .views import PostDetail, PostList
+from .views import (
+    PostCreate,
+    PostDelete,
+    PostDetail,
+    PostList,
+    PostUpdate,
+)
 
 urlpatterns = [
     path("", PostList.as_view(), name="post_list"),
     path(
+        "create/", PostCreate.as_view(), name="post_create"
+    ),
+    path(
         "<int:year>/<int:month>/<str:slug>/",
         PostDetail.as_view(),
         name="post_detail",
+    ),
+    path(
+        "<int:year>/<int:month>/<str:slug>/delete/",
+        PostDelete.as_view(),
+        name="post_delete",
+    ),
+    path(
+        "<int:year>/<int:month>/<str:slug>/update/",
+        PostUpdate.as_view(),
+        name="post_update",
     ),
 ]
