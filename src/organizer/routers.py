98cf5d509645de6639184eca46a1b2e980335f1b
Andrew Pinkham <code@andrewsforge.com>
2018-11-28 20:29:18 -0500

    🎂 Advanced: use ViewSet and Router for NewsLinks

--- src/organizer/routers.py
+++ src/organizer/routers.py
@@ -1,26 +1,46 @@
 """URL Paths and Routers for Organizer App"""
-from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from .views import NewsLinkAPIDetail, NewsLinkAPIList
-from .viewsets import StartupViewSet, TagViewSet
+from .viewsets import (
+    NewsLinkViewSet,
+    StartupViewSet,
+    TagViewSet,
+)
+
+
+class NewsLinkRouter(SimpleRouter):
+    """Override the SimpleRouter for articles
+
+    DRF's routers expect there to only be a single variable
+    for finding objects. However, our NewsLinks needs
+    two! We therefore override the Router's behavior to
+    make it do what we want.
+
+    The big question: was it worth switching to a ViewSet
+    and Router over our previous config for this?
+    """
+
+    def get_lookup_regex(self, *args, **kwargs):
+        """Return regular expression pattern for URL path
+
+        This is the (rough) equivalent of the simple path:
+            <str:startup_slug>/<str:newslink_slug>
+        """
+        return (
+            r"(?P<startup_slug>[^/.]+)/"
+            r"(?P<newslink_slug>[^/.]+)"
+        )
+
 
 api_router = SimpleRouter()
 api_router.register("tag", TagViewSet, base_name="api-tag")
 api_router.register(
     "startup", StartupViewSet, base_name="api-startup"
 )
-api_routes = api_router.urls
 
-urlpatterns = api_routes + [
-    path(
-        "newslink/",
-        NewsLinkAPIList.as_view(),
-        name="api-newslink-list",
-    ),
-    path(
-        "newslink/<str:startup_slug>/<str:newslink_slug>/",
-        NewsLinkAPIDetail.as_view(),
-        name="api-newslink-detail",
-    ),
-]
+nl_router = NewsLinkRouter()
+nl_router.register(
+    "newslink", NewsLinkViewSet, base_name="api-newslink"
+)
+
+urlpatterns = api_router.urls + nl_router.urls
